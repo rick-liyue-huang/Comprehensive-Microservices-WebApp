1afb781e865879f2916b8c63d7c6931598a2f785
Rick Huang <rick.liyue.huang@gmail.com>
2026-04-21 21:35:25 +1000

    add mongo connection

--- OrdersMicroservice/OrdersMicroservice.BusinessLogicLayer/obj/Debug/net10.0/OrdersMicroservice.BusinessLogicLayer.AssemblyInfo.cs
+++ OrdersMicroservice/OrdersMicroservice.BusinessLogicLayer/obj/Debug/net10.0/OrdersMicroservice.BusinessLogicLayer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("OrdersMicroservice.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22f67aadbc4e57ff2e46d3baf385e79c7e7afd50")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3834361389be912fb332400ba23f35b66ca6f8df")]
 [assembly: System.Reflection.AssemblyProductAttribute("OrdersMicroservice.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("OrdersMicroservice.BusinessLogicLayer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- OrdersMicroservice/OrdersMicroservice.DataAccessLayer/DependencyInjection.cs
+++ OrdersMicroservice/OrdersMicroservice.DataAccessLayer/DependencyInjection.cs
@@ -1,5 +1,6 @@
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using MongoDB.Driver;
 
 namespace OrdersMicroservice.DataAccessLayer;
 
@@ -7,6 +8,14 @@ public static class DependencyInjection
 {
     public static IServiceCollection AddDataAccessLayer(this IServiceCollection services, IConfiguration configuration)
     {
+        string connectionString = configuration.GetConnectionString("OrdersDb")!;
+        
+        services.AddSingleton<IMongoClient>(new MongoClient(connectionString));
+        services.AddScoped<IMongoDatabase>(provider =>
+        {
+            IMongoClient client = provider.GetRequiredService<IMongoClient>();
+            return client.GetDatabase("ecommerce_order_db");
+        });
         return services;
     }
 }
